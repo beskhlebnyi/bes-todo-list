fe528f1f168a5a64378f6030fd1723f97e046080
Yaroslav <beskhlebnyi@gmail.com>
2018-03-28 18:39:05 +0300

    Create associations

--- db/migrate/20180328153513_create_tasks.rb
+++ db/migrate/20180328153513_create_tasks.rb
@@ -3,6 +3,7 @@ class CreateTasks < ActiveRecord::Migration[5.1]
     create_table :tasks do |t|
       t.string :content
       t.boolean :status
+      t.belongs_to :list, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328150305) do
+ActiveRecord::Schema.define(version: 20180328153513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,11 @@ ActiveRecord::Schema.define(version: 20180328150305) do
   create_table "tasks", force: :cascade do |t|
     t.string "content"
     t.boolean "status"
+    t.bigint "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
   end
 
+  add_foreign_key "tasks", "lists"
 end
