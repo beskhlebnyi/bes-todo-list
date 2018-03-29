93f012c9b30ea87505bfc231bbfbeaf4d5b6b61c
Yaroslav <beskhlebnyi@gmail.com>
2018-03-29 17:22:36 +0300

    Remove scaffold for tasks and last migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists do
-    resources :tasks
-  end
+  resources :lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
 end
