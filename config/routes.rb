650ececf3070cdc4ef7a9a4a1686d20f121d5ff1
Yaroslav <beskhlebnyi@gmail.com>
2018-03-30 13:02:59 +0300

    Change tasks/index rendering method

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :lists do
     resources :tasks
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'lists#index'
 
-  get '/list-tasks/:id', to: 'lists#list_tasks', as: :render_tasks
+  root to: 'lists#index'
 end
