73ec934b65b03b42fcf67c24ab6ffe90f51fa558
Yaroslav <beskhlebnyi@gmail.com>
2018-03-31 12:37:42 +0300

    Reafactoring list test

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
+  subject{ described_class.new(title: "some title") }
+
   it "valid with valid attributes" do
-    expect(List.new(title: 'not blank title')).to be_valid
+    subject.title = "some title"
+    expect(subject).to be_valid
   end
 
   it "not valid without a title" do
-    list = List.new(title: nil)
-    expect(list).to_not be_valid
+    subject.title = nil
+    expect(subject).to_not be_valid
   end
 end
