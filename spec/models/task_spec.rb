0a69697fbc2c8bb75b109bde5c5b2dac161c4335
Yaroslav <beskhlebnyi@gmail.com>
2018-04-01 10:27:53 +0300

    Refacoring rspec for task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,34 +5,25 @@ RSpec.describe Task, type: :model do
     @list = List.create( title: "Some title" ) 
   end
 
+  subject{ described_class.new(
+    content: "Some content",
+    status: false,
+    important: true,
+    list: @list
+  )}
+
   it "is valid with list" do
-    task = Task.new(
-      content: "Some content",
-      status: false,
-      important: true,
-      list: @list
-    )
-    expect(task).to be_valid
+    expect(subject).to be_valid
   end
 
   it "is invalid without list" do
-    task = Task.new(
-      content: "Some content",
-      status: false,
-      important: true,
-      list: nil
-    )
-    expect(task).not_to be_valid
+    subject.list = nil
+    expect(subject).not_to be_valid
   end
 
   it "is invalid without content" do
-    task = Task.new(
-      content: nil,
-      status: false,
-      important: true,
-      list: @list
-    )
-    expect(task).not_to be_valid
+    subject.content = nil
+    expect(subject).not_to be_valid
   end
 
 end
