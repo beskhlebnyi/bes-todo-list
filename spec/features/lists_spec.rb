44d98ec1e90067065d39b6877d106f92f0a16b07
Yaroslav <beskhlebnyi@gmail.com>
2018-04-09 20:40:30 +0300

    Base feature specs for list

--- spec/features/lists_spec.rb
+++ spec/features/lists_spec.rb
@@ -1,16 +1,41 @@
 require 'rails_helper'
 
-RSpec.feature "Lists", type: :feature do
-  scenario "guest creates a new list",  js: true do
+RSpec.feature "Lists", type: :feature, js: true do
+  scenario "user create a new list" do
     visit root_path
     
     expect{
       click_link "New List"
-      fill_in "Title", with: "some list"
+      fill_in "Title", with: "Some list"
       click_button "Create List"
       visit root_path
     }.to change(List.all, :count).by(1)
 
     expect(page).to have_content "some list"
+  end
+
+  scenario "user show list of tasks" do
+    create(:list, title: "Some list")
+    visit root_path
+    click_link "Some list"
+    expect(page).to have_content "Listing tasks"
+  end
+
+  scenario "user delete a list" do
+    visit root_path
+
+    expect{
+      click_link "Destroy"
+      visit root_path
+    }.to change(List.all, :count).by(-1)
+  end
+
+  scenario "user update a list" do
+    visit root_path
+    click_link "Edit"
+    fill_in "Title", with: "Edited list"
+    click_button "Update List"
+    visit root_path
+    expect(page).to have_content "Edited list"
   end
 end
