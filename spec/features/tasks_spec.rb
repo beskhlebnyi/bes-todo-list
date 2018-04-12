87e56408d569f9c3bf09624f34b345fa8b4bfe88
Yaroslav <beskhlebnyi@gmail.com>
2018-04-12 13:28:27 +0300

    Refactor list and task for using let!

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,16 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature, js: true do
-  
-  before do
-    @task = create(:task, :with_list)
-  end
+  let!(:task) { create(:task, :with_list) }
 
   scenario "user create a new task" do
     visit root_path
 
     expect {
-      click_link @task.list.title
+      click_link task.list.title
       click_link "New Task"
       fill_in "Content", with: "some task"
       check "Status"
@@ -22,9 +19,9 @@ RSpec.feature "Tasks", type: :feature, js: true do
 
   scenario "user edit a task" do
     visit root_path
-    click_link @task.list.title
+    click_link task.list.title
     
-    within "#task-#{@task.id}" do
+    within "#task-#{task.id}" do
       click_link "Edit"
     end
     
@@ -33,8 +30,31 @@ RSpec.feature "Tasks", type: :feature, js: true do
     check "Important"
     click_button "Update Task"
     visit root_path
-    click_link @task.list.title
+    click_link task.list.title
     
     expect(page).to have_content("some new task")
+  end
+
+  scenario "user delete a task" do
+    visit root_path
+    click_link task.list.title
+    
+    within "#task-#{task.id}" do
+      click_link "Destroy"
+    end
+    
+    visit root_path
+    click_link task.list.title
+    
+    expect(page).not_to have_content(task.content)
+  end
+
+  scenario "user show a task" do
+    visit root_path
+    click_link task.list.title
+    click_link task.content
+
+    expect(page).to have_content("Task")
+    expect(page).to have_content("Content:#{task.content}")
   end
 end
