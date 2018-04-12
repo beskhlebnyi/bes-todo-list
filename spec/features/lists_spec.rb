62430fd98030b093edadc898804e45af9c23c886
Yaroslav <beskhlebnyi@gmail.com>
2018-04-12 18:15:36 +0300

    Add object id in html.slim and specs for list

--- spec/features/lists_spec.rb
+++ spec/features/lists_spec.rb
@@ -16,6 +16,62 @@ RSpec.feature "Lists", type: :feature, js: true do
     expect(page).to have_content "some list"
   end
 
+  context "with a empty title" do
+    scenario "user can't create new list" do
+      visit root_path
+
+      expect{
+        click_link "New List"
+        fill_in "Title", with: ""
+        click_button "Create List"
+      }.not_to change(List.all, :count)
+  
+      expect(page).to have_content "Title can't be blank"
+    end
+
+    scenario "user can't update a list" do
+      visit root_path
+
+      within "#list-#{list.id}" do
+        click_link "Edit" 
+      end
+
+      fill_in "Title", with: ""
+      click_button "Update List"
+      
+      expect(page).to have_content "Title can't be blank"
+    end
+  end
+
+  context "with same title" do
+    scenario "user can't create new list" do
+      create(:list, title: "same title")
+      visit root_path
+
+      expect{
+        click_link "New List"
+        fill_in "Title", with: "same title"
+        click_button "Create List"
+      }.not_to change(List.all, :count)
+  
+      expect(page).to have_content "Title has already been taken"
+    end
+
+    scenario "user can't update list" do
+      create(:list, title: "same title")
+      visit root_path
+
+      within "#list-#{list.id}" do
+        click_link "Edit" 
+      end
+
+      fill_in "Title", with: "same title"
+      click_button "Update List"
+      
+      expect(page).to have_content "Title has already been taken"
+    end
+  end
+
   scenario "user show list of tasks" do
     visit root_path
     click_link "Some list"
