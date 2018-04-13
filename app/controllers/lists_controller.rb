29dec2c7a778118ebbb08be31efb74d633b24387
Yaroslav <beskhlebnyi@gmail.com>
2018-04-13 13:48:33 +0300

    Change update action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -51,7 +51,7 @@ class ListsController < ApplicationController
     respond_to do |format|
       if @list.update(list_params)
         # format.html { redirect_to root_path, remote: true, notice: 'List was successfully updated.' }
-        format.js { render 'list_index.js.erb' }
+        format.js # { render 'list_index.js.erb' }
         format.json { render :show, status: :ok, location: @list }
       else
         # format.html { redirect_to root_path, notice: "#{@list.errors.full_messages.first}" }
