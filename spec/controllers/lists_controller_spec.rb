d1a925b5956127f7752a858767b0d8a6a1f56e77
Yaroslav <beskhlebnyi@gmail.com>
2018-04-13 10:28:03 +0300

    Change list specs for js actions responces

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe ListsController, type: :controller do
+RSpec.describe ListsController, type: :controller, js: true do
   let(:new_list) { create(:list) }
 
   describe "#index" do
     it "responds successfully" do
-      get :index
+      get :index, xhr: true
       expect(response).to be_success
     end
 
     it "returns a 200 response" do
-      get :index
+      get :index, xhr: true
       expect(response).to have_http_status "200"
     end
   end
@@ -19,7 +19,7 @@ RSpec.describe ListsController, type: :controller do
     it "adds as list" do
       list_params = attributes_for(:list)
       expect {
-        post :create, params: { list: list_params }
+        post :create, params: { list: list_params }, xhr: true
       }.to change(List.all, :count).by(1)
     end
   end
@@ -27,7 +27,7 @@ RSpec.describe ListsController, type: :controller do
   describe "#update" do
     it "updates a list" do
       list_params = attributes_for(:list, title: "Some new title")
-      post :update, params: { id: new_list.id, list: list_params  }
+      post :update, params: { id: new_list.id, list: list_params  }, xhr: true
       expect(new_list.reload.title).to eq "Some new title"
     end
   end
@@ -36,7 +36,7 @@ RSpec.describe ListsController, type: :controller do
     it "deletes a list" do
       some_list = create(:list)
       expect {
-        delete :destroy, params: { id: some_list.id }
+        delete :destroy, params: { id: some_list.id }, xhr: true
       }.to change(List.all, :count).by(-1)
     end
   end
