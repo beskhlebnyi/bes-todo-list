bb6d0b006c46e99722951cd8e89776e982afa33a
Yaroslav <beskhlebnyi@gmail.com>
2018-04-13 09:46:12 +0300

    Remake all lists actions to respond to js

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
   before_action :set_list , only: [:show, :edit, :update, :destroy, :list_tasks]
-  before_action :set_lists, only: [:main_page, :index, :destroy]
+  before_action :set_lists, only: [:main_page, :index, :create, :update, :destroy]
 
 
   def main_page
@@ -8,7 +8,9 @@ class ListsController < ApplicationController
   end
 
   def index
-    respond_to(&:js)
+    respond_to do |format|
+      format.js { render 'list_index.js.erb' }
+    end
   end
 
   def list_tasks
@@ -33,10 +35,12 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
     respond_to do |format|
       if @list.save
-        format.html { redirect_to root_path, remote: true, notice: "List was successfully created." }
+        # format.html { redirect_to root_path, remote: true, notice: "List was successfully created." }
+        format.js { render 'list_index.js.erb' }
         format.json { render :show, status: :created, location: @list }
       else
-        format.html { redirect_to root_path, notice: "#{@list.errors.full_messages.first}" }
+        # format.html { redirect_to root_path, notice: "#{@list.errors.full_messages.first}" }
+        format.js { render 'list_index.js.erb' }
         format.json { render json: @list.errors, status: :unprocessable_entity }
       end
     end
@@ -45,10 +49,12 @@ class ListsController < ApplicationController
   def update
     respond_to do |format|
       if @list.update(list_params)
-        format.html { redirect_to root_path, remote: true, notice: 'List was successfully updated.' }
+        # format.html { redirect_to root_path, remote: true, notice: 'List was successfully updated.' }
+        format.js { render 'list_index.js.erb' }
         format.json { render :show, status: :ok, location: @list }
       else
-        format.html { redirect_to root_path, notice: "#{@list.errors.full_messages.first}" }
+        # format.html { redirect_to root_path, notice: "#{@list.errors.full_messages.first}" }
+        format.js { render 'list_index.js.erb' }
         format.json { render json: @list.errors, status: :unprocessable_entity }
       end
     end
@@ -58,7 +64,7 @@ class ListsController < ApplicationController
     @list.destroy
     respond_to do |format|
       # format.html { redirect_to root_path, notice: 'List was successfully deleted.' }
-      format.js
+      format.js { render 'list_index.js.erb' }
       format.json { head :no_content }
     end
   end
