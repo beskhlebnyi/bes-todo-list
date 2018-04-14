36aa0a6dbc05b638c036850be74f696c9de5bd99
Yaroslav <beskhlebnyi@gmail.com>
2018-04-14 10:19:49 +0300

    Change update action for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -39,10 +39,12 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to root_path, notice: 'Task was successfully updated.' }
+        # format.html { redirect_to root_path, notice: 'Task was successfully updated.' }
+        format.js
         format.json { render :show, status: :ok, location: @task }
       else
-        format.html { redirect_to root_path, notice: "#{@task.errors.full_messages.first}" }
+        # format.html { redirect_to root_path, notice: "#{@task.errors.full_messages.first}" }
+        format.js 
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
