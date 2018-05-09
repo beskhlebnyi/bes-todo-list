bc81e07773485d8fdf29a31072b5c399aa5e0dfa
be5 <beskhlebnyi@gmail.com>
2018-05-09 20:20:38 +0300

    Change deadline type to date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508133430) do
+ActiveRecord::Schema.define(version: 20180509171808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20180508133430) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "important"
-    t.datetime "deadline", null: false
+    t.date "deadline", null: false
     t.index ["list_id"], name: "index_tasks_on_list_id"
   end
 
