8f92bfaf2603596849d4c21487bb5161875731c2
be5 <beskhlebnyi@gmail.com>
2018-05-14 14:00:30 +0300

    Add remind field and config job to use it

--- app/jobs/send_remind_mail_job.rb
+++ app/jobs/send_remind_mail_job.rb
@@ -3,8 +3,10 @@ class SendRemindMailJob < ApplicationJob
 
   def perform
     Task.all.each do |task|
-      if task.deadline_soon?
+      if task.deadline_soon? && !task.reminded
         RemindLetterMailer.remind_email(task.id).deliver_now
+        task.reminded = true
+        task.save
       end
     end
   end
