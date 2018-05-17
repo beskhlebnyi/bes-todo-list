55890f9a10e0d3cf6619f8120537e4669872f27f
be5 <beskhlebnyi@gmail.com>
2018-05-17 17:31:09 +0300

    Update deadlone_soon? method

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,10 @@ class Task < ApplicationRecord
   validates  :content, presence: true, uniqueness: { scope: :list_id }
 
   def deadline_soon?
-    self.deadline ? self.deadline <= DateTime.now.utc - 2.hours && self.important : false
+    self.deadline ? self.deadline >= DateTime.now - 2.hours && self.important : false
   end
 end
+
+##NOTE
+## get the timezone offset in hours:
+## Task.last.deadline.strftime('%z')[0..2].to_i 
