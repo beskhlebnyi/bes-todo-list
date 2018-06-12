be7032cc4843bf72987cf745ba8e0ca534476a0b
be5 <beskhlebnyi@gmail.com>
2018-06-12 11:31:32 +0300

    change errors displaying for registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,13 +11,36 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-   super   
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      @notice = resource.errors.full_messages.first
+      respond_to do |format|
+        format.js { render 'shared/notice.js.erb' }
+      end
+
+      clean_up_passwords resource
+      set_minimum_password_length
+      # TODO: Check respond_with functonal
+      # respond_with resource  end   
+    end
   end
 
   # GET /resource/edit
   def edit
     super
-    console
   end
 
   # PUT /resource
