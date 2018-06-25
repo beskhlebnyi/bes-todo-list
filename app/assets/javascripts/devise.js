e04c098b7221db704d1780e0885f2f0b4eb06596
be5 <beskhlebnyi@gmail.com>
2018-06-25 09:06:24 +0300

    add animation for  forms

--- app/assets/javascripts/devise.js
+++ app/assets/javascripts/devise.js
@@ -1,3 +1,13 @@
-$(document).on('turbolinks:load', function() {
+$(document).on('turbolinks:load', function () {
   $(".login-box").slideDown(800);
+});
+
+$(document).on('turbolinks:load', function() {
+  $(".submit-login").click(function () {
+    $(".login-box").slideUp(800);
+  });
+
+  $(".disapear").click(function () {
+    $(".login-box").hide();
+  });
 });
