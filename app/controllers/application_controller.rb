1519c4a3d2d5675e1692660c75cfac5cab2d6249
be5 <beskhlebnyi@gmail.com>
2018-06-26 10:28:25 +0300

    add before action to app_controller for setting locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  
+  before_action :set_locale
+ 
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+  
   def set_flash_error(object)
     object.errors.full_messages.each { |error| flash.now[:error] = error }
   end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
 end
