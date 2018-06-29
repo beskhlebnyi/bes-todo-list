544467f09038c6f96b3563cb0d94899e11572602
be5 <beskhlebnyi@gmail.com>
2018-06-29 12:37:00 +0300

    Add errors displaying for edit user page

--- app/assets/javascripts/devise.js
+++ app/assets/javascripts/devise.js
@@ -1,3 +1,8 @@
 $(document).on('turbolinks:load', function () {
   $(".login-box").slideDown(800);
+  
+  $(".alert").delay(4000).slideUp(200, function() {
+    $(this).alert('close');
+  });
+
 });
