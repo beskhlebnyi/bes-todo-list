ff9c403bd4b75f7fbfb26e1f90bbf4bf5fccf425
be5 <beskhlebnyi@gmail.com>
2018-07-10 11:23:44 +0300

    Change user from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ApplicationRecord
 
   has_many  :lists, dependent: :destroy
 
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+   end
   end
 end
