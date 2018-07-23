96e2f70b921592c20d4e337f70c3c8f6b5149dec
be5 <beskhlebnyi@gmail.com>
2018-07-23 15:04:07 +0300

    Rename task migration to document

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2018_07_02_154310) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "documents", force: :cascade do |t|
+    t.bigint "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_documents_on_list_id"
+  end
+
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -54,19 +61,6 @@ ActiveRecord::Schema.define(version: 2018_07_02_154310) do
     t.index ["user_id"], name: "index_shared_lists_on_user_id"
   end
 
-  create_table "files", force: :cascade do |t|
-    t.string "content"
-    t.boolean "status"
-    t.bigint "list_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "important"
-    t.boolean "reminded", default: false
-    t.datetime "deadline"
-    t.string "timezone", default: "UTC"
-    t.index ["list_id"], name: "index_files_on_list_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -80,13 +74,12 @@ ActiveRecord::Schema.define(version: 2018_07_02_154310) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "timezone", default: "UTC"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "documents", "lists"
   add_foreign_key "lists", "users"
   add_foreign_key "shared_lists", "lists"
   add_foreign_key "shared_lists", "users"
-  add_foreign_key "files", "lists"
 end
