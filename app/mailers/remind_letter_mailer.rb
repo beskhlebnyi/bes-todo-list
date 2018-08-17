72ff5960f114aa2bb74258ced163c5037f45bf6a
be5 <beskhlebnyi@gmail.com>
2018-08-17 15:08:31 +0300

    using sendgrin in development env

--- app/mailers/remind_letter_mailer.rb
+++ app/mailers/remind_letter_mailer.rb
@@ -2,10 +2,7 @@ class RemindLetterMailer < ApplicationMailer
 
   def remind_email(task_id)
     @task = Task.find(task_id)
-    mail(to: 'beskhlebnyi@gmail.com', subject: 'Amazon SES mail')
-    ##TODO
-    ##uncomment next line after increase sending limit
-    ##mail(to: @task.list.user.email, subject: 'You have some task to do!')
+    mail(to: @task.list.user.email, subject: 'You have some task to do!')
   end
 
 end
